0659bbde1462a551a92037b0fff4e03127dc510d
Toneejake <johnkervinevangelista@gmail.com>
2026-02-22 19:53:55 +0800

    migration: also add type and order columns to assessment_questions

--- prisma/migrations/20260520000000_add_name_fields/migration.sql
+++ prisma/migrations/20260520000000_add_name_fields/migration.sql
@@ -2,3 +2,7 @@
 ALTER TABLE "users" ADD COLUMN IF NOT EXISTS "firstName" TEXT NOT NULL DEFAULT '';
 ALTER TABLE "users" ADD COLUMN IF NOT EXISTS "lastName" TEXT NOT NULL DEFAULT '';
 ALTER TABLE "users" ADD COLUMN IF NOT EXISTS "middleName" TEXT;
+
+-- AlterTable: Add type and order columns to assessment_questions table
+ALTER TABLE "assessment_questions" ADD COLUMN IF NOT EXISTS "type" TEXT NOT NULL DEFAULT 'preTest';
+ALTER TABLE "assessment_questions" ADD COLUMN IF NOT EXISTS "order" INTEGER NOT NULL DEFAULT 0;
